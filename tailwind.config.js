46682903b74f6acb891a6ca89efc3b8575fa96b8
Tibebe Solomon <87706650+Tibex88@users.noreply.github.com>
2024-02-19 17:02:32 +0300

    add assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        celeste:'#d4d5ce',
+        gray:'#808080',
+        desert_storm:'#f9f9f7',
+        green_spring:'#b4b6af',
+        bitter:'#7b7e72',
+      }
+    },
   },
   plugins: [],
 }
